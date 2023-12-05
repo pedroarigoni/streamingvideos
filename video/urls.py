473feff18716d224240bd11be400c2fe05b4aa9d
pedroargioni <pedroargioni@gmail.com>
2023-12-05 15:26:53 -0300

    configured dynamic links for DetailView

--- video/urls.py
+++ video/urls.py
@@ -3,8 +3,11 @@
 from django.urls import path, include
 from .views import HomeVideos, HomePage, DetalhesVideos
 
+
+app_name = 'video'
+
 urlpatterns = [
-    path('', HomePage.as_view()),
-    path('videos/', HomeVideos.as_view()),
-    path('videos/<int:pk>', DetalhesVideos.as_view())
+    path('', HomePage.as_view(), name='homepage'),
+    path('videos/', HomeVideos.as_view(), name='homevideos'),
+    path('videos/<int:pk>', DetalhesVideos.as_view(), name='detalhesvideos')
 ]
