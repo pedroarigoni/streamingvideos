861e02375343b05a7dfdd16d25dfc6b6193c45cd
pedroargioni <pedroargioni@gmail.com>
2023-12-05 14:47:53 -0300

    created DetailView

--- video/urls.py
+++ video/urls.py
@@ -1,9 +1,10 @@
 # url - view - template
 
 from django.urls import path, include
-from .views import HomeVideos, HomePage
+from .views import HomeVideos, HomePage, DetalhesVideos
 
 urlpatterns = [
     path('', HomePage.as_view()),
-    path('videos/', HomeVideos.as_view())
+    path('videos/', HomeVideos.as_view()),
+    path('videos/<int:pk>', DetalhesVideos.as_view())
 ]
--- video/views.py
+++ video/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Filme
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
 class HomePage(TemplateView):
@@ -9,4 +9,8 @@ class HomePage(TemplateView):
 # url - view - html
 class HomeVideos(ListView):
     template_name = "homevideos.html"
+    model = Filme
+
+class DetalhesVideos(DetailView):
+    template_name = "detalhesvideo.html"
     model = Filme
