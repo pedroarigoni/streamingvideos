9acd09ce1800e305c8c0cd906137e32a5e28974e
pedroargioni <pedroargioni@gmail.com>
2023-12-05 15:52:53 -0300

    created and configured table Episodio

--- video/models.py
+++ video/models.py
@@ -18,3 +18,13 @@ class Filme(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+# Episódios
+class Episodio(models.Model):
+    filme = models.ForeignKey("Filme", related_name="episodios", on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    video = models.URLField()
+
+    def __str__(self):
+        return self.filme.titulo + ' - ' + self.titulo
