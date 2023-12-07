ad203cbb6053c785732b436f04a5ce8fa818e908
pedroargioni <pedroargioni@gmail.com>
2023-12-07 10:58:20 -0300

    created carousel for homevideos

--- video/new_context.py
+++ video/new_context.py
@@ -1,9 +1,9 @@
 from .models import Filme
 
 def lista_videos_recentes(request):
-    lista_videos = Filme.objects.all().order_by("-data_criacao")[0:10]
+    lista_videos = Filme.objects.all().order_by("-data_criacao")[0:8]
     return {"lista_videos_recentes": lista_videos}
 
 def lista_mais_assistidos(request):
-    lista_videos = Filme.objects.all().order_by("-visualizacoes")[0:10]
+    lista_videos = Filme.objects.all().order_by("-visualizacoes")[0:8]
     return {"lista_mais_assistidos": lista_videos}
