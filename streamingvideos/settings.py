c74653ed87728afb9510c3818797e4a3857f7f5d
pedroargioni <pedroargioni@gmail.com>
2024-01-04 09:07:56 -0300

    configured CSRF

--- streamingvideos/settings.py
+++ streamingvideos/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7mbb_z)b*jvcgi0@d&h^klo0o(5&jjuz1m6ts1!qc31w9gsc=!'
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://streamingvideos-production.up.railway.app/']
+else:
+    SECRET_KEY = 'django-insecure-7mbb_z)b*jvcgi0@d&h^klo0o(5&jjuz1m6ts1!qc31w9gsc=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://streamingvideos-production.up.railway.app/", "localhost", "127.0.0.1"]
 
 # Application definition
 
